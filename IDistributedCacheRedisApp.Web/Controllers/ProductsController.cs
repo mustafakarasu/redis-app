fe63ec9a335357d2adf6722a106dbe05eb3d0c78
Mustafa Karasu <mustaffa.karasu@hotmail.com>
2024-06-14 18:39:35 +0300

    The project for the distributed cache instance is complete.

---
+++ IDistributedCacheRedisApp.Web/Controllers/ProductsController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using IDistributedCacheRedisApp.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+
+namespace IDistributedCacheRedisApp.Web.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly IDistributedCache _distributedCache;
+
+        public ProductsController(IDistributedCache distributedCache)
+        {
+            _distributedCache = distributedCache;
+        }
+
+        public Task<IActionResult> Index()
+        {
+            var cacheEntryOptions = new DistributedCacheEntryOptions
+            {
+                AbsoluteExpiration = DateTime.Now.AddMinutes(30)
+            };
+
+            var product = new Product()
+            {
+                Id = 1,
+                Name = "Kalem",
+                Price = 100
+            };
+
+            string jsonProduct = JsonConvert.SerializeObject(product);
+
+            Byte[] byteProduct = Encoding.UTF8.GetBytes(jsonProduct);
+
+            _distributedCache.Set("product:1", byteProduct);
+
+            //await _distributedCache.SetStringAsync("product:1", jsonProduct, cacheEntryOptions);
+
+            return Task.FromResult<IActionResult>(View());
+        }
+
+        public IActionResult Show()
+        {
+            Byte[] byteProduct = _distributedCache.Get("product:1");
+            string jsonProduct = Encoding.UTF8.GetString(byteProduct);
+            var product = JsonConvert.DeserializeObject<Product>(jsonProduct);
+
+            ViewBag.product = product;
+            
+            return View();
+        }
+
+        public IActionResult Remove()
+        {
+            _distributedCache.Remove("name");
+
+            return View();
+        }
+
+        public IActionResult ImageUrl()
+        {
+            byte[] resimByte = _distributedCache.Get("resim");
+
+            return File(resimByte, "image/jpg");
+        }
+
+        public IActionResult ImageCache()
+        {
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images/download.jpg");
+            byte[] imageByte = System.IO.File.ReadAllBytes(path);
+            _distributedCache.Set("resim", imageByte);
+
+            return View();
+        }
+    }
+}
