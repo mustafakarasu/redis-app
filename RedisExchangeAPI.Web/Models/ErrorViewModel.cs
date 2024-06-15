b2da89a8b4b6792e0573b1ab60c3e4a422ba26d2
Mustafa Karasu <mustaffa.karasu@hotmail.com>
2024-06-15 20:46:44 +0300

    MVC project and RedisService class were created for Redis Exchange.

---
+++ RedisExchangeAPI.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace RedisExchangeAPI.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
