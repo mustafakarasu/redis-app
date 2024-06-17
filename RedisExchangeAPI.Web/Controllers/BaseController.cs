6eb8ff0df0b2d970c522b60a21454019db1a6930
Mustafa Karasu <mustaffa.karasu@hotmail.com>
2024-06-17 17:02:12 +0300

    A base controller was created for all Controller classes to use.

---
+++ RedisExchangeAPI.Web/Controllers/BaseController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RedisExchangeAPI.Web.Services;
+using StackExchange.Redis;
+
+namespace RedisExchangeAPI.Web.Controllers
+{
+    public abstract class BaseController : Controller
+    {
+        protected readonly RedisService RedisService;
+        protected readonly IDatabase Db;
+
+        protected BaseController(RedisService redisService)
+        {
+            RedisService = redisService;
+            Db = RedisService.GetDb(1);
+        }
+    }
+}
