11990d42d13e7a0f12fb7c8cbbf68c4b7c824fdc
Mustafa Karasu <mustaffa.karasu@hotmail.com>
2024-06-20 21:32:30 +0300

    Examples were made in the Redis.Web project for Hash and Set types.

---
+++ RedisExchangeAPI.Web/Controllers/HashTypeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RedisExchangeAPI.Web.Services;
+using StackExchange.Redis;
+
+namespace RedisExchangeAPI.Web.Controllers
+{
+    public class HashTypeController : BaseController
+    {
+        private string HashKey { get; set; } = "Sozluk";
+
+        public HashTypeController(RedisService redisService) : base(redisService)
+        {
+        }
+
+        public IActionResult Index()
+        {
+            var list = new Dictionary<string, string>();
+
+            if (Db.KeyExists(HashKey))
+            {
+                Db.HashGetAll(HashKey).ToList().ForEach(x =>
+                {
+                    list.Add(x.Name, x.Value);
+                });
+            }
+
+            return View(list);
+        }
+
+        [HttpPost]
+        public IActionResult Add(string name, string val)
+        {
+            Db.HashSet(HashKey, name, val);
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult DeleteItem(string name)
+        {
+            Db.HashDelete(HashKey, name);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ RedisExchangeAPI.Web/Controllers/SetTypeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RedisExchangeAPI.Web.Services;
+using StackExchange.Redis;
+
+namespace RedisExchangeAPI.Web.Controllers
+{
+    public class SetTypeController : Controller
+    {
+        private readonly RedisService _redisService;
+
+        private readonly IDatabase _db;
+
+        private string _listKey = "HashNames";
+
+        public SetTypeController(RedisService redisService)
+        {
+            _redisService = redisService;
+            _db = _redisService.GetDb(2);
+        }
+
+        public IActionResult Index()
+        {
+            var namesList = new HashSet<string>();
+
+            if (_db.KeyExists(_listKey))
+            {
+                _db.SetMembers(_listKey).ToList().ForEach(x =>
+                {
+                    namesList.Add(x.ToString());
+                });
+            }
+
+            return View(namesList);
+        }
+
+        [HttpPost]
+        public IActionResult Add(string name)
+        {
+            _db.KeyExpire(_listKey, DateTime.Now.AddMinutes(5));
+            _db.SetAdd(_listKey, name);
+
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> DeleteItem(string name)
+        {
+            await _db.SetRemoveAsync(_listKey, name);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
